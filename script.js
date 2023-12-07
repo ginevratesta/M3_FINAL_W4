33ea2f1aa7a7ac135c4845d0954bc1a15642304c
Ginevra Testa <ginevra99testaa@gmail.com>
2023-12-07 16:58:01 +0100

    last commit

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function fadeInElement(element) {
-    element.style.opacity = '1';
-    element.style.transform = 'translateX(0)';
-  }
+  element.style.opacity = "1";
+  element.style.transform = "translateX(0)";
+}
 
-  function fadeOutElement(element) {
-    element.style.opacity = '0';
-    element.style.transform = 'translateX(-50px)';
-  }
+function fadeOutElement(element) {
+  element.style.opacity = "0";
+  element.style.transform = "translateX(-50px)";
+}
 
-  function checkScroll() {
-    const carousels = document.querySelectorAll('.carousel');
+function checkScroll() {
+  const carousels = document.querySelectorAll(".carousel");
 
-    carousels.forEach(carousel => {
-      const rect = carousel.getBoundingClientRect();
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  carousels.forEach((carousel) => {
+    const rect = carousel.getBoundingClientRect();
+    const windowHeight =
+      window.innerHeight || document.documentElement.clientHeight;
 
-      if (rect.top >= 0 && rect.top <= windowHeight) {
-        fadeInElement(carousel);
-      } else {
-        fadeOutElement(carousel);
-      }
-    });
-  }
+    if (rect.top >= 0 && rect.top <= windowHeight) {
+      fadeInElement(carousel);
+    } else {
+      fadeOutElement(carousel);
+    }
+  });
+}
 
-  window.addEventListener('scroll', checkScroll);
-  window.addEventListener('resize', checkScroll);
-  window.addEventListener('load', checkScroll);
+window.addEventListener("scroll", checkScroll);
+window.addEventListener("resize", checkScroll);
+window.addEventListener("load", checkScroll);
